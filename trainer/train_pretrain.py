b5c8ff444c1d12d15a95fb58c251cb23a7093708
rubyway <pszzbb@hotmail.com>
2025-08-13 21:53:02 +0800

    Update train_pretrain.py

--- trainer/train_pretrain.py
+++ trainer/train_pretrain.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
         wandb = None
 
     model, tokenizer = init_model(lm_config)
-    train_ds = PretrainDataset(args.data_path, tokenizer, max_length=args.max_seq_len)
+    train_ds = PretrainDataset('/kaggle/input/minimind-dataset/pretrain_hq.jsonl', tokenizer, max_length=args.max_seq_len)
     train_sampler = DistributedSampler(train_ds) if ddp else None
     train_loader = DataLoader(
         train_ds,
